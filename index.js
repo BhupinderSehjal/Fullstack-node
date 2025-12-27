e4b3ab2e18508d798b428cbdec5e72d288f4304f
Bhupinder Sehjal <101923266+BhupinderSehjal@users.noreply.github.com>
2025-12-27 16:01:46 +0530

    Initial Commit

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 
+const {format, addDays,subDays} = require('date-fns');
+
+const now = new Date();
+console.log("Today is " , format(now,"yyyy-MM-dd"));
+
+const nextWeek  = addDays(now, 7);
+console.log("Next week is " , format(nextWeek,"yyyy-MM-dd"));
+
+const previousWeek = subDays(now, 7);
+console.log("Previous week was " , format(previousWeek,"yyyy-MM-dd"));
